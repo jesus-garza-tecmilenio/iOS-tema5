2df1ee509567cbe7768bf5a00989d345387c4ace
jesus-garza-gro <jesus.garza.gro@hotmail.com>
2025-09-09 17:45:14 -0600

    Initial Commit

---
+++ Tema5Swift/Tema5SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tema5SwiftApp.swift
+//  Tema5Swift
+//
+//  Created by JESUS GARZA on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Tema5SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
